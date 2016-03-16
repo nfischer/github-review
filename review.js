1156a70628b8da297bfb0cd6bcdfae49bb1fcaed
Nate Fischer <ntfschr@gmail.com>
2016-03-15 17:17:36 -0700

    Works with checking out master branch now

--- review.js
+++ review.js
@@ -24,60 +24,60 @@ if (args.length < 1) {
 
 // Clean up the url
 // Of the form: https://github.com/shelljs/benchmarks/pull/6
-var urlMatch = args[0].match(/^https:\/\/github.com\/(.*)\/(.*)\/pull\/(\d+)/);
-if (!urlMatch) {
+var repoName;
+var remoteName;
+var urlMatch = args[0].match(/^https:\/\/github.com\/(.+)\/(.+)\/pull\/(\d+)/);
+if (urlMatch) {
+  var prUrl = urlMatch[0];
+  var ownerName = urlMatch[1];
+  repoName = urlMatch[2];
+  var prNumber = 'PR_' + urlMatch[3];
+
+  // Pull the web page, parse it, and determine the correct URLs
+  echo('Pulling your URL...');
+  echo(prUrl);
+  var html = exec('curl ' + prUrl).output;
+  echo('URL successfully fetched');
+
+  // For parsing with xpath
+  var parserArgs = {
+    errorHandler:{} // silence error messages
+  };
+  var doc = new dom(parserArgs).parseFromString(html);
+  var nodes = xpath.select('//head/title', doc);
+  var page_title = nodes[0].firstChild.data.trim();
+  echo(page_title);
+
+  var re = /[\u0000-\u007F]*/g;
+  var matches = page_title.match(re);
+  var description = matches[0].trim();
+
+  nodes = xpath.select('//div/div/a[@class="author pull-header-username css-truncate css-truncate-target expandable"]', doc);
+  var author = nodes[0].firstChild.data.trim();
+
+  nodes = xpath.select('//div/div/span[@class="commit-ref current-branch css-truncate user-select-contain expandable"]/span', doc);
+
+  var branch_name;
+  var upstream;
+  if (nodes.length === 4) {
+    // remote
+    remoteName = nodes[2].firstChild.data.trim();
+    branch_name = nodes[3].firstChild.data.trim();
+    upstream = 'https://github.com/' + ownerName + '/' + repoName + '.git';
+  } else {
+    // from the same repo
+    remoteName = ownerName;
+    branch_name = nodes[1].firstChild.data.trim();
+  }
+} else if (!(urlMatch = args[0].match(/^https:\/\/github.com\/(.+)\/([^\/]+)/))) {
   console.error('URL format is incorrect');
   exit(1);
 }
-
-var prUrl = urlMatch[0];
-var ownerName = urlMatch[1];
-var repoName = urlMatch[2];
-var prNumber = 'PR_' + urlMatch[3];
-
-// Pull the web page, parse it, and determine the correct URLs
-echo('Pulling your URL...');
-echo(prUrl);
-var html = exec('curl ' + prUrl).output;
-echo('URL successfully fetched');
-
-// For parsing with xpath
-var parserArgs = {
-  errorHandler:{} // silence error messages
-};
-var doc = new dom(parserArgs).parseFromString(html);
-var nodes;
-
-nodes = xpath.select('//head/title', doc);
-var page_title = nodes[0].firstChild.data.trim();
-echo(page_title);
-
-var re = /[\u0000-\u007F]*/g;
-var matches = page_title.match(re);
-var description = matches[0].trim();
-
-nodes = xpath.select('//div/div/a[@class="author pull-header-username css-truncate css-truncate-target expandable"]', doc);
-var author = nodes[0].firstChild.data.trim();
-
-nodes = xpath.select('//div/div/span[@class="commit-ref current-branch css-truncate user-select-contain expandable"]/span', doc);
-
-var remoteName;
-var branch_name;
-var upstream;
-if (nodes.length === 4) {
-  // remote
-  remoteName = nodes[2].firstChild.data.trim();
-  branch_name = nodes[3].firstChild.data.trim();
-  // upstream = 'https://github.com/' + nodes[0].firstChild.data + '/shelljs.git';
-  upstream = 'https://github.com/' + ownerName + '/' + repoName + '.git';
-} else {
-  // from the same repo
-  remoteName = ownerName;
-  branch_name = nodes[1].firstChild.data.trim();
-}
-
 // Setup directories
-var name = prNumber;
+remoteName = remoteName || urlMatch[1];
+repoName = repoName || urlMatch[2];
+var topDir = prNumber || 'review_' + repoName;
+var name = topDir;
 rm('-Rf', name);
 mkdir(name);
 cd(name);
@@ -86,11 +86,14 @@ mkdir(name);
 cd(name);
 
 // Clone the person's repo
-echo('Cloning the branch...');
-var cloneCmd = 'git clone -b ' + branch_name + ' https://github.com/' +
+echo('Cloning the repository...');
+var cloneCmd = 'git clone' +
+    (branch_name ? ' -b ' + branch_name : '') +
+    ' https://github.com/' +
     remoteName + '/' + repoName + '.git';
 echo(cloneCmd);
 exec(cloneCmd);
+
 
 cd(ls()[0]); // There's only one file, and it's our folder
 if (upstream) {
@@ -138,5 +141,5 @@ if (repoName.toLowerCase() === 'shelljs') {
     echo('This PR passes initial tests!');
   }
 }
-echo('cd ' + prNumber + '/    or');
-echo('cd ' + prNumber + '/node_modules/' + repoName + '/');
+echo('cd ' + topDir + '/    or');
+echo('cd ' + topDir + '/node_modules/' + repoName + '/');
